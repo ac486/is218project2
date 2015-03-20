2d7ea09a6b04b3f32f919a225013e403bd62c6dc
Arthur Chen <ac486@afsconnect1.njit.edu>
2015-03-20 00:27:13 -0400

    project set up

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <html>
 <p>test</p>
+<?php
+   echo'<a href="?page=php">PHP Menu</a></br>';
+
+   $obj = new $REQUEST['page'];
+   class page{
+      function __contruct(){
+         echo 'I am the' . $_REQUEST['page'],'page';
+      }
+   }
+
+   class php extends page {
+      function __construct(){
+         echo 'This is an object:',$_REQUEST['page'],'page';
+      }
+      
+   }
 
 
 
 
 
 
+
+
+
+
+?>
 </html>
